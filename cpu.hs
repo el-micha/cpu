e064067d9a4b8f609bd2da318fcc820857ee6728
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-05-10 08:31:49 +0200

    brainstorming

--- cpu.hs
+++ cpu.hs
@@ -1,9 +1,25 @@
 
 import qualified Data.Vector as V
 
+type Vector = V.Vector
+
+data Bit = Zero | One
+type Word' = Vector Bit
+type RAM = Vector Word'
+type Register = Word'
+type DataRegisters = [Register]
+type PC = Register
+type StackPointer = Register
+type StatusRegister = Register
 
 
+split1 :: Char -> String -> [String]
+--        a    -> b      -> m b
+split1 = undefined
 
+split :: [Char] -> String -> [String]
+--       m a    -> b      -> m b
+split = undefined
 
 
 
