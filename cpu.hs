26b45188998d4d66c77ba6a7947ef5777f6ae19c
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-05-24 20:38:04 +0200

    synch

--- cpu.hs
+++ cpu.hs
@@ -12,14 +12,13 @@ type PC = Register
 type StackPointer = Register
 type StatusRegister = Register
 
+-- R opcode 6, rs 5, rt 5 rd 5, shamt 5, funct 6
+-- I opcode 6, rs 5, rt 5, immediate 16
+-- J opcode 6, address 26
+-- addressing mode: base + displacement 
 
-split1 :: Char -> String -> [String]
---        a    -> b      -> m b
-split1 = undefined
 
-split :: [Char] -> String -> [String]
---       m a    -> b      -> m b
-split = undefined
+
 
 
 
