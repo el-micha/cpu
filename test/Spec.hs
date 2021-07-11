2968a6da463283250937db7e72a8d8992ba08a88
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-07-12 01:07:21 +0200

    test setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+
+import Memory
+import Data.Maybe
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+--main = putStrLn "testing"
+main = do putStr $ show t1
+
+
+t1 = writeWord emptyMemory (fromJust (mkAddress 3)) (fromJust (mkDataWord [1,2,2,3]))
