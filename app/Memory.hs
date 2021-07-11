8ad7a301581f4580d6cebec679f4968f877b18dc
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-07-12 00:00:11 +0200

    DataWord, Address, Memory

--- app/Memory.hs
+++ app/Memory.hs
@@ -1,22 +1,78 @@
 module Memory
-( mkMemory
+( dataWordLength
+, emptyDataWord
+, mkDataWord
+, memorySize
+, mkAddress
+, emptyMemory
+, mkMemory
 , readWord
 , writeWord
 ) where
 
 import Data.List
 
-data Memory = Memory [[Int]]
+
+
+-------------------------------------------------------------------------------
+-- DataWords
+
+dataWordLength :: Int
+dataWordLength = 4
+
+-- newtype to be able to enforce constraints: length
+newtype DataWord = DataWord [Int] 
+
+instance Show DataWord where
+  show (DataWord x) = show x
+
+emptyDataWord :: DataWord
+emptyDataWord = DataWord $ replicate dataWordLength 0
+
+mkDataWord :: [Int] -> Maybe DataWord
+mkDataWord xs = if length xs /= dataWordLength then Nothing else Just (DataWord xs)
+
+-------------------------------------------------------------------------------
+-- Addresses
+
+memorySize :: Int
+memorySize = 8
+
+-- check positive and bounded at construction
+newtype Address = Address Int
+
+mkAddress :: Int -> Maybe Address
+mkAddress x = if x < 0 || x >= memorySize then Nothing else Just (Address x)
+
+-------------------------------------------------------------------------------
+-- Memory
+
+data Memory = Memory [DataWord]
 
 instance Show Memory where
   show (Memory words) = intercalate "\n" (map show words)
 
-emptyWord = [0,0,0,0]
+emptyMemory :: Memory
+emptyMemory = Memory $ replicate memorySize emptyDataWord
 
-mkMemory :: Int -> Memory
-mkMemory n = Memory $ replicate n emptyWord
+mkMemory :: [DataWord] -> Maybe Memory
+mkMemory words = if len > memorySize then Nothing else Just (Memory $ words ++ rest)
+  where len = length words
+        rest = replicate (memorySize - len) emptyDataWord
+
+readWord :: Memory -> Address -> DataWord
+readWord (Memory words) (Address n) = words !! n
+
+writeWord :: Memory -> Address -> DataWord -> Memory
+writeWord (Memory words) (Address n) dataWord = Memory $ replace words n dataWord
+  where replace :: [a] -> Int -> a -> [a]
+        replace (xx:xs) 0 x = x : xs
+        replace (xx:xs) n x = xx : (replace xs (n-1) x)
+        
+-------------------------------------------------------------------------------
 
 
-readWord = undefined
 
-writeWord = undefined
+
+
+
