332dad3ab872c3c943c7f8e1b9a6e4b6406768b7
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-07-12 11:39:13 +0200

    more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,12 @@
 import Memory
 import Data.Maybe
 
-main :: IO ()
+--main :: IO ()
 --main = putStrLn "testing"
-main = do putStr $ show t1
+main = sequence $ map (putStr . (++"\n\n") . show) [t1, t2]
 
 
 t1 = writeWord emptyMemory (fromJust (mkAddress 3)) (fromJust (mkDataWord [1,2,2,3]))
+
+t2 = fromJust $ mkMemory $ map (fromJust . mkDataWord) [[1,2,3,4],[2,4,6,8],[9,8,7,6],[0,1,0,1]]
+
